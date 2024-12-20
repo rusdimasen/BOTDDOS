1b131c02e540564384c4bb75b4f528937b1d8c8d
rusdimasen <storezresdi@gmail.com>
2024-12-20 13:19:10 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -16,48 +16,43 @@ function createBot(initialNumber) {
       }
     }
 
-    if (config.altening) {
-      snekfetch.get(`http://api.thealtening.com/v1/generate?token=${config.altening_token}&info=true`).then((n) => {
-        var mineflayer = require('mineflayer');
-        var bot = mineflayer.createBot({
-          host: config.ip,
-          port: config.port,
-          username: n.body.token,
-          version: config.version
-        });
+    var mineflayer = require("mineflayer");
+    var bot = mineflayer.createBot({
+      host: config.ip,
+      port: config.port,
+      username: `${config.crackedusernameprefix}${currentNumber}`,
+      version: config.version,
+    });
 
-        handleBotEvents(bot);
-      });
-    } else {
-      var mineflayer = require('mineflayer');
-      var bot = mineflayer.createBot({
-        host: config.ip,
-        port: config.port,
-        username: `${config.crackedusernameprefix}${currentNumber}`,
-        version: config.version
-      });
-
-      handleBotEvents(bot);
-    }
+    handleBotEvents(bot, currentNumber);
   }
 
-  function handleBotEvents(bot) {
-    bot.on('login', () => {
+  function handleBotEvents(bot, botNumber) {
+    bot.on("login", () => {
       console.log(`Logged in as: ${bot.username}`);
       activeBots.push(bot); // Tambahkan bot ke daftar aktif
+
+      // Register atau Login
+      setTimeout(() => {
+        bot.chat(`/register ${config.password} ${config.password}`);
+        bot.chat(`/login ${config.password}`);
+      }, 1000); // Tunggu sebentar sebelum mengirimkan perintah
 
       // Rejoin setelah waktu tertentu
       setTimeout(() => {
         bot.quit("Rejoining...");
         activeBots = activeBots.filter((b) => b !== bot); // Hapus dari daftar aktif
-        currentNumber++; // Ganti username
         loginBot(); // Login ulang
       }, config.rejoinintervalms);
     });
 
-    bot.on('error', (err) => console.log(err));
-    bot.on('kicked', (reason) => {
+    bot.on("error", (err) => console.log(err));
+    bot.on("kicked", (reason) => {
       console.log("Kicked for", reason);
+      activeBots = activeBots.filter((b) => b !== bot); // Hapus dari daftar aktif
+    });
+
+    bot.on("end", () => {
       activeBots = activeBots.filter((b) => b !== bot); // Hapus dari daftar aktif
     });
   }
